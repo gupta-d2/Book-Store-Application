26f08ddc18459562b8dca88f4f4cfc7e46edac04
Deepak Gupta <dgupta@tractorsupply.com>
2021-08-27 01:47:52 +0530

    Added validation and swagger config for book store application

--- src/main/java/com/api/bookstore/app/exception/BookStoreException.java
+++ src/main/java/com/api/bookstore/app/exception/BookStoreException.java
@@ -2,10 +2,6 @@ package com.api.bookstore.app.exception;
 
 
 import lombok.*;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
